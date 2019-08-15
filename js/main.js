dcec11519fd28a55afc50a9d2758e66c9997dbd6
Victor Melnikov <sukhvost@yandex.ru>
2019-08-15 23:02:27 +0300

    scroll and create media.css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	// плавное перемещение страницы к нужному блоку
-	$(".scroll").click(function () {
+	$(".go__to").click(function () {
 		elementClick = $(this).attr("href");
 		destination = $(elementClick).offset().top;
 		$("body,html").animate({scrollTop: destination }, 800);
